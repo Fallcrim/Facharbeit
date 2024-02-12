36b36659d2dcbb50da029d1785d41985f8ea8c2e
Fallcrim <80700363+Fallcrim@users.noreply.github.com>
2024-02-12 18:54:18 +0100

    Started to work on Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,39 @@
-public class Client {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client implements Runnable {
+
+    private Socket clientSocket;
+    private BufferedReader eingehend;
+    private PrintWriter ausgehend;
+    private boolean clientAktiv;
+
+    @Override
+    public void run() {
+        try {
+            this.clientSocket = new Socket("localhost", 2007);
+            this.ausgehend = new PrintWriter(clientSocket.getOutputStream(), true);
+            this.eingehend = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException e) {
+            // TODO: verarbeiten
+        }
+    }
+
+    private class EingabeHandler implements Runnable {
+        @Override
+        public void run() {
+            try {
+                BufferedReader eingabeReader = new BufferedReader(new InputStreamReader(System.in));
+                while (clientAktiv) {
+                    String nachricht = eingabeReader.readLine();
+
+                }
+            } catch (IOException e) {
+                // TODO: verarbeiten
+            }
+        }
+    }
 }
